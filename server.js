5af54ea2628792e1834774fe5b160cf673497e85
leesh <lsh8250a@gmail.com>
2020-01-20 23:12:47 +0900

    Add Get Customers REST API Module

--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id':1,
+            'image':'https://placeimg.com/64/64/animals/1',
+            'name': '홍길동',
+            'birthday':'921111',
+            'gender':'남자',
+            'job':'대학생'
+          },
+          {
+            'id':2,
+            'image':'https://placeimg.com/64/64/animals/2',
+            'name': '한예슬',
+            'birthday':'921212',
+            'gender':'여자',
+            'job':'배우'
+          },
+          {
+            'id':3,
+            'image':'https://placeimg.com/64/64/animals/3',
+            'name': '박나래',
+            'birthday':'921214',
+            'gender':'여자',
+            'job':'개그우먼'
+          }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
